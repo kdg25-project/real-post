25d9a243cb0c19a7d85be37656684a70d5071fee
kato <katoharuki25kdgn@gmail.com>
2025-11-19 18:04:15 +0900

    ボツ

--- src/app/user/home/page.tsx
+++ src/app/user/home/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/layouts/Header/Header";
+import { Button } from "@/components/ui/button"
 
 export default function HomePage() {
     return (
         <div>
             <Header searchArea={true} />
+            <Button variant="outline">Button</Button>
         </div>
     );
 }
