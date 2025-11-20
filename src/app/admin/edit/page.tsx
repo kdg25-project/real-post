dbfa9d7484fb739444809acabfe0a0b5d537170b
uenoreo <yokunokatamari@ymobile.ne.jp>
2025-11-20 22:42:19 +0900

    createImageUpload

--- src/app/admin/edit/page.tsx
+++ src/app/admin/edit/page.tsx
@@ -1,9 +1,12 @@
 'use client';
 
+import { useState } from "react";
 import TextForm from "@/components/layouts/TextForm";
+import ImageUpload from "@/components/layouts/ImageUpload";
 import { CategoryForm,NativeSelectOption, NativeSelectOptGroup } from "@/components/layouts/CategoryForm";
 
 export default function EditPage() {
+    const [preview, setPreview] = useState<string | null>(null);
     return (
         <div>
             <TextForm label="店舗名" type="text" placeholder="例 ご飯大好きの会" />
@@ -17,6 +20,16 @@ export default function EditPage() {
                     <NativeSelectOption value="5">その他</NativeSelectOption>
                 </NativeSelectOptGroup>
             </CategoryForm>
+            <TextForm label="SNS" type="text" placeholder="例 https://example.com/" />
+            <ImageUpload
+                label="店舗画像"
+                preview={preview ?? undefined}
+                onChange={(file) => {
+                    if (!file) return;
+                    const url = URL.createObjectURL(file);
+                    setPreview(url);
+                }}
+            />
         </div>
     )
 }
