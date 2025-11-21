d73b53cb12c7cb2e120110c0173641e0af888f27
uenoreo <yokunokatamari@ymobile.ne.jp>
2025-11-21 11:57:15 +0900

    fix

--- src/app/admin/edit/page.tsx
+++ src/app/admin/edit/page.tsx
@@ -6,9 +6,14 @@ import ImageUpload from "@/components/layouts/ImageUpload";
 import { CategoryForm,NativeSelectOption, NativeSelectOptGroup } from "@/components/layouts/CategoryForm";
 
 export default function EditPage() {
-    const [preview, setPreview] = useState<string | null>(null);
+    const [preview1, setPreview1] = useState<string | null>(null);
+    const [preview2, setPreview2] = useState<string | null>(null);
+
     return (
-        <div>
+        <div className="flex flex-col justify-center gap-6 pb-[104px]">
+            <div className="flex items-center justify-between w-full h-[292px] bg-gray">
+                <h1>仮</h1>
+            </div>
             <TextForm label="店舗名" type="text" placeholder="例 ご飯大好きの会" />
             <TextForm label="住所" type="text" placeholder="例 名古屋市中村区日本橋1-1" />
             <CategoryForm>
@@ -23,11 +28,20 @@ export default function EditPage() {
             <TextForm label="SNS" type="text" placeholder="例 https://example.com/" />
             <ImageUpload
                 label="店舗画像"
-                preview={preview ?? undefined}
+                preview={preview1 ?? undefined}
                 onChange={(file) => {
                     if (!file) return;
                     const url = URL.createObjectURL(file);
-                    setPreview(url);
+                    setPreview1(url);
+                }}
+            />
+            <ImageUpload
+                label="グッズ画像"
+                preview={preview2 ?? undefined}
+                onChange={(file) => {
+                    if (!file) return;
+                    const url = URL.createObjectURL(file);
+                    setPreview2(url);
                 }}
             />
         </div>
