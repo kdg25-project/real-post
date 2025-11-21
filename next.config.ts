314dd4210a2a8dbe733663d63cecf33de60c7011
tah5882 <me@tah5882.dev>
2025-11-21 19:29:58 +0900

    feat: company_profileにplace_urlカラムを追加し、データ構造を強化

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [process.env.NEXT_PUBLIC_R2_PUBLIC_URL || "localhost"],
+  },
 };
 
 export default nextConfig;
